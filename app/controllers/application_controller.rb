9efa4d2a8c9a314b3d2a29206e76230020841ef1
吉川健史 <yoshikawatakeshi@yoshikawatakeshinoMac-mini.local>
2021-07-09 18:01:07 +0900

    [Add] Ransackを導入しヘッダーの検索フォームを作成した.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :set_search
+
+  def set_search
+    @q = Shop.ransack(params[:q])
+    @shops = @q.result.includes(:games).page(params[:page])
+  end
 end
