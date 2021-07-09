3536b4f3d22d5bf0942851b588c6ef252f61ee58
‘吉川健史’ <71423029+TakeshiYoshi@users.noreply.github.com>
2021-07-09 17:00:32 +0900

    [Add] Shops#show ビューを作成した.

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,5 +1,9 @@
 class ShopsController < ApplicationController
   def index
-    @shops = Shop.all.includes(:games).page(params[:page])
+    @shops = Shop.where(prefecture: Prefecture.find_by(name: '東京都')).includes(:games).page(params[:page])
+  end
+
+  def show
+    @shop = Shop.includes(:games).find(params[:id])
   end
 end
