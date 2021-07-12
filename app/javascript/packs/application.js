51f1f3c9179419f0bd6047e2d3338b02462cea15
‘吉川健史’ <71423029+TakeshiYoshi@users.noreply.github.com>
2021-07-12 14:22:09 +0900

    [Fix] imageファイルをAssetsからwebpackerへ移動した.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,3 +7,6 @@ import "../stylesheets/application";
 import '@fortawesome/fontawesome-free/js/fontawesome';
 import '@fortawesome/fontawesome-free/js/solid';
 import '@fortawesome/fontawesome-free/js/regular';
+
+const images = require.context('../images', true)
+const imagePath = (name) => images(name, true)
