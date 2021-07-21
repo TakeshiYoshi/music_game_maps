e0e83e1735a5968d82b8aa1906715def742049d2
‘吉川健史’ <71423029+TakeshiYoshi@users.noreply.github.com>
2021-07-21 14:58:10 +0900

    [Add] ユーザー登録ページのモックを作成した.

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   root to: 'shops#index'
   resources :shops, only: %i[show]
   resource :filter, only: %i[create destroy]
+  resources :users, only: %i[create]
+  get 'signup', to: 'users#new'
   post 'cities', to: 'filters#cities_select'
   post 'near_shops_search', to: 'filters#near_shops_search'
-  delete 'clear_near_shops_search', to: 'filters#clear_near_shops_search'
   post 'set_location', to: 'filters#set_location'
+  delete 'clear_near_shops_search', to: 'filters#clear_near_shops_search'
 end
