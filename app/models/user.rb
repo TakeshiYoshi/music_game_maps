19dc37fb35385b3ed6f6905f4ac65c08551ab54c
吉川健史 <yoshikawatakeshi@yoshikawatakeshinoMac-mini.local>
2021-07-21 17:55:03 +0900

    [Add] ユーザー登録ページを実装した.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,12 @@ class User < ApplicationRecord
 
   authenticates_with_sorcery!
 
-  validates :password, length: { minimum: 8 }, if: -> { new_record? || changes[:crypted_password] }
+  VALID_PASSWORD_REGEX = /(?=.*?[a-z])(?=.*?[A-Z])[a-zA-Z\d!?_.$&%\-]{8,30}/.freeze
+  validates :password, length: { in: 8..30 }, format: { with: VALID_PASSWORD_REGEX }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
 
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+  VALID_EMAIL_REGEX = /[a-zA-Z0-9_.+-]+@([a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]*\.)+[a-zA-Z]{2,}/.freeze
   validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
   validates :nickname, presence: true, length: { maximum: 30 }
   validates :description, length: { maximum: 300 }
