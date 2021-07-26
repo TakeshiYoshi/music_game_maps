3c85e1cc0d677e4e6d57757ef2bb0cf27cf74afe
吉川健史 <yoshikawatakeshi@yoshikawatakeshinoMac-mini.local>
2021-07-26 10:48:22 +0900

    [Add] フラッシュメッセージの表示を実装した.

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -5,14 +5,15 @@ class UserSessionsController < ApplicationController
     @user = login(params[:email], params[:password], params[:remember])
 
     if @user
-      redirect_to root_url
+      redirect_to root_url, success: t('.success')
     else
+      flash.now[:danger] = t('.danger')
       render :new
     end
   end
 
   def destroy
     logout
-    redirect_to root_url
+    redirect_to root_url, success: t('.success')
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       create_playing_games
-      redirect_to login_url
+      redirect_to login_url, success: t('.success')
     else
       render :new
     end
