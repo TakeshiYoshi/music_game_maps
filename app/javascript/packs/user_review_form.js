accf57a9a84fe6c8ade1d828b23e58964628db68
吉川健史 <71423029+TakeshiYoshi@users.noreply.github.com>
2021-09-06 16:45:21 +0900

    [Fix] ユーザー投稿フォームの本文のバリデーション処理を修正した.

--- app/javascript/packs/user_review_form.js
+++ app/javascript/packs/user_review_form.js
@@ -27,8 +27,7 @@ new Vue({
   },
   computed: {
     bodyValidCheck: function() {
-      const bodyReg = /^.{1,1000}$/;
-      this.bodyError = bodyReg.test(this.bodyField) ? '' : '本文は1~1000文字で入力してください。'
+      this.bodyError = this.bodyField.length <= 1000 ? '' : '本文は1~1000文字で入力してください。'
     },
     bodyNullCheck: function() {
       this.bodyError = this.bodyField ? '' : '本文を入力してください。'
