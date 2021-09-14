53c341523cc91044a87f581939166a6f05ae7a3c
吉川健史 <71423029+TakeshiYoshi@users.noreply.github.com>
2021-09-15 01:11:49 +0900

    [Add] TwitterログインボタンにTwitterロゴを追加した.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,9 +4,7 @@ require("channels")
 
 import "bootstrap"
 import "../stylesheets/application";
-import '@fortawesome/fontawesome-free/js/fontawesome';
-import '@fortawesome/fontawesome-free/js/solid';
-import '@fortawesome/fontawesome-free/js/regular';
+import '@fortawesome/fontawesome-free/js/all';
 
 const images = require.context('../images', true)
 const imagePath = (name) => images(name, true)
