b5ba114c93e8d8a741054cf1ee4c6ef9c1006728
吉川健史 <71423029+TakeshiYoshi@users.noreply.github.com>
2021-10-30 20:28:04 +0900

    [Add] GameMachineDecoratorに対するspecを追記した.

--- spec/decorators/game_machine_decorator_spec.rb
+++ spec/decorators/game_machine_decorator_spec.rb
@@ -3,7 +3,23 @@
 require 'rails_helper'
 
 RSpec.describe GameMachineDecorator do
-  let(:game_machine) { GameMachine.new.extend GameMachineDecorator }
-  subject { game_machine }
-  it { should be_a GameMachine }
+  let!(:game_machine) { create(:game_machine) }
+  before { ActiveDecorator::Decorator.instance.decorate game_machine }
+
+  describe 'count_info_exist?' do
+    # countが99の時は台数情報不明と定義しています。
+    context 'countが99のとき' do
+      it 'falseが返される' do
+        game_machine.update(count: 99)
+        expect(game_machine.count_info_exist?).to eq false
+      end
+    end
+
+    context 'countが99以外のとき' do
+      it 'trueが返される' do
+        game_machine.update(count: 0)
+        expect(game_machine.count_info_exist?).to eq true
+      end
+    end
+  end
 end
