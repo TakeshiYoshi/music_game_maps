482cade784113f1e933b75621957534c19fb7fee
吉川健史 <71423029+TakeshiYoshi@users.noreply.github.com>
2021-11-03 01:22:36 +0900

    [Fix] admin.jsを削除した.

--- app/javascript/packs/admin.js
+++
@@ -1,7 +0,0 @@
-require("@rails/ujs").start()
-require("@rails/activestorage").start()
-require("channels")
-
-import "bootstrap"
-import "../stylesheets/admin";
-import '@fortawesome/fontawesome-free/js/all';
