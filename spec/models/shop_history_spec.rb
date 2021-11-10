ead5ef75963cc145d4ba188f4c539357c8f54750
吉川健史 <71423029+TakeshiYoshi@users.noreply.github.com>
2021-11-10 19:26:39 +0900

    [Add] ShopHistoryモデルのspecを追加した. GameMachineモデルのspecを追記した.

--- spec/models/shop_history_spec.rb
+++ spec/models/shop_history_spec.rb
@@ -1,4 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe ShopHistory, type: :model do
+  it 'websiteは正しいフォーマットで入力されていること' do
+    shop_history = build(:shop_history, website: 'hogehoge://www.hoge.fuga')
+    shop_history.valid?
+    expect(shop_history.errors[:website]).to include('は不正な値です')
+  end
+
+  it 'statusの初期値がdraftであること' do
+    shop_history = build(:shop_history)
+    expect(shop_history.status).to eq('draft')
+  end
 end
