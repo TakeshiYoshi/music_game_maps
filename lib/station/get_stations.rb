77e5c9ee2df08c92436642285f6f2425fbeea4d1
吉川健史 <71423029+TakeshiYoshi@users.noreply.github.com>
2022-03-19 23:14:44 +0900

    最寄り駅情報取得時にモデル作成するように実装した

--- lib/station/get_stations.rb
+++ lib/station/get_stations.rb
@@ -2,7 +2,9 @@ require 'nokogiri'
 require 'open-uri'
 
 def get_stations
-  get_station Shop.find(483)
+  (1..10).each do |n|
+    get_station Shop.find(n) if Shop.find(n).present?
+  end
 end
 
 def get_station(shop)
@@ -10,14 +12,21 @@ def get_station(shop)
   url = "http://express.heartrails.com/api/json?method=getStations&x=#{shop.lng.to_f}&y=#{shop.lat.to_f}"
   page = URI.parse(url).open.read
   json = JSON.parse(page)
+  stations_data = json['response']['station']
+  stations_data.each do |station_data|
+    station_name = station_data['name']
+    prefecture = station_data['prefecture']
+    line_name = station_data['line']
+    lng = station_data['x']
+    lat = station_data['y']
+    distance = station_data['distance']
 
-  stations = json['response']['station']
-  stations.each do |station|
-    name = station['name']
-    prefecture = station['prefecture']
-    line = station['line']
-    lng = station['x']
-    lat = station['y']
-    distance = station['distance']
+    line = Line.find_or_create_by(name: line_name)
+    station = Station.find_or_create_by(name: station_name) do |s|
+      s.lng = lng
+      s.lat = lat
+      s.prefecture = Prefecture.find_by(name: prefecture)
+      s.line = line
+    end
   end
 end
