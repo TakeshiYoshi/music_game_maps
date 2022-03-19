7ddac769761d3c8bd7a9210baed295acce581a55
吉川健史 <71423029+TakeshiYoshi@users.noreply.github.com>
2022-03-20 00:13:10 +0900

    店舗・駅情報を作成できるようにした

--- lib/station/get_stations.rb
+++ lib/station/get_stations.rb
@@ -2,17 +2,23 @@ require 'nokogiri'
 require 'open-uri'
 
 def get_stations
-  (1..10).each do |n|
-    get_station Shop.find(n) if Shop.find(n).present?
+  Shop.all.each do |shop|
+    next if shop.shop_stations.present?
+
+    sleep 0.3
+    get_station shop
   end
 end
 
 def get_station(shop)
+  puts "#{shop.name}の最寄り駅情報を取得する"
   # 最寄り駅情報を取得
   url = "http://express.heartrails.com/api/json?method=getStations&x=#{shop.lng.to_f}&y=#{shop.lat.to_f}"
   page = URI.parse(url).open.read
   json = JSON.parse(page)
   stations_data = json['response']['station']
+
+  # 最寄り駅情報を保存
   stations_data.each do |station_data|
     station_name = station_data['name']
     prefecture = station_data['prefecture']
@@ -21,12 +27,18 @@ def get_station(shop)
     lat = station_data['y']
     distance = station_data['distance']
 
+    # 路線情報を保存
     line = Line.find_or_create_by(name: line_name)
-    station = Station.find_or_create_by(name: station_name) do |s|
-      s.lng = lng
-      s.lat = lat
-      s.prefecture = Prefecture.find_by(name: prefecture)
-      s.line = line
-    end
+    # 駅情報を保存
+    station = Station.find_or_initialize_by(name: station_name)
+    station.update(
+      lng: lng,
+      lat: lat,
+      prefecture: Prefecture.find_by(name: prefecture),
+      line: line,
+    )
+    # 店舗・駅情報を保存
+    shop_station = ShopStation.find_or_initialize_by(shop: shop, station: station)
+    shop_station.update(distance: distance)
   end
 end
