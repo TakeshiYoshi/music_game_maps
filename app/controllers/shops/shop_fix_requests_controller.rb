d3731f8eda1fd2e28c1948f5388ad03a0bc38279
吉川健史 <71423029+TakeshiYoshi@users.noreply.github.com>
2022-04-13 23:46:54 +0900

    createアクション作成

--- app/controllers/shops/shop_fix_requests_controller.rb
+++ app/controllers/shops/shop_fix_requests_controller.rb
@@ -1,8 +1,27 @@
 class Shops::ShopFixRequestsController < ApplicationController
+  before_action :set_shop, only: %i[new create]
+
   def new
-    @shop = Shop.find(params[:shop_id])
     @shop_fix_request = @shop.shop_fix_requests.build
   end
 
-  def create; end
+  def create
+    @shop_fix_request = @shop.shop_fix_requests.build(shop_fix_request_params)
+    if @shop_fix_request.save
+      redirect_to shop_path(@shop), success: t('.success')
+    else
+      flash.now[:danger] = t('.danger')
+      render :new
+    end
+  end
+
+  private
+
+  def set_shop
+    @shop = Shop.find(params[:shop_id])
+  end
+
+  def shop_fix_request_params
+    params.require(:shop_fix_request).permit(:not_exist, :duplicate, :fix_shop_info, :body)
+  end
 end
--- spec/models/shop_fix_request_spec.rb
+++ spec/models/shop_fix_request_spec.rb
@@ -1,14 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe ShopFixRequest, type: :model do
-  it '各報告は必須項目であること' do
-    shop_fix_request = build(:shop_fix_request, not_exist: nil, duplicate: nil, fix_shop_info: nil)
-    shop_fix_request.valid?
-    expect(shop_fix_request.errors[:not_exist]).to include('を入力してください')
-    expect(shop_fix_request.errors[:duplicate]).to include('を入力してください')
-    expect(shop_fix_request.errors[:fix_shop_info]).to include('を入力してください')
-  end
-
   it '本文が1000文字を超える場合無効であること' do
     shop_fix_request = build(:shop_fix_request, body: 'a'*1001)
     shop_fix_request.valid?
