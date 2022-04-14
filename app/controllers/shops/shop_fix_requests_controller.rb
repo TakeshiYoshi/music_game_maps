a8766720d640e5cffc40a977aa6a4f1464ecf64a
吉川健史 <71423029+TakeshiYoshi@users.noreply.github.com>
2022-04-14 17:51:02 +0900

    fix lint

--- app/controllers/shops/shop_fix_requests_controller.rb
+++ app/controllers/shops/shop_fix_requests_controller.rb
@@ -7,8 +7,6 @@ class Shops::ShopFixRequestsController < ApplicationController
 
   def create
     @shop_fix_request = @shop.shop_fix_requests.build(shop_fix_request_params)
-    puts 'a'*100
-    puts shop_fix_request_params
     if values_present? && @shop_fix_request.save
       redirect_to shop_path(@shop), success: t('.success')
     else
@@ -29,6 +27,6 @@ class Shops::ShopFixRequestsController < ApplicationController
 
   def values_present?
     # 無記入投稿防止用
-    shop_fix_request_params.values.map{ |value| value === 'true' ? value : nil }.compact.present?
+    shop_fix_request_params.values.map { |value| value == 'true' ? value : nil }.compact.present?
   end
 end
